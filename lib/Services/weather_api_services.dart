4f46869066eb0e1f5c7986054f16097ec258487c
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2024-02-06 22:19:14 +0500

    Updated

--- lib/Services/weather_api_services.dart
+++ lib/Services/weather_api_services.dart
@@ -4,13 +4,18 @@ import 'package:http/http.dart' as http;
 
 class WeatherApiServices{
 
-  Map <String, dynamic> data = {};
+   Map <String, dynamic> data = {};
    Map <String, dynamic> current = {};
    Map <String, dynamic> location= {};
 
+   Map <String, dynamic> forecast = {};
+   Map <String, dynamic> forecastday= {};
+   Map <String, dynamic> astro= {};
+   Map <String, dynamic> hour= {};
+
 
   Future<void> fetchWorldWeatherApi() async{
-    final response = await http.get(Uri.parse("http://api.weatherapi.com/v1/current.json?key=258f004eb4f44042b54180807241601&q=Karachi&aqi=no"));
+    final response = await http.get(Uri.parse("http://api.weatherapi.com/v1/forecast.json?key=258f004eb4f44042b54180807241601&q=Karachi&aqi=no"));
     
     if(response.statusCode==200){
           if (kDebugMode) {
@@ -20,7 +25,14 @@ class WeatherApiServices{
        data = jsonDecode(response.body.toString());
       current = data['current']; 
       location = data['location'];
+
+      forecast = data['forecast'];
+      forecastday = data['forecastday']; 
+      astro = data['astro'];
+      hour = data['hour'];
+
+      
      
     }
     }
-  }
+} 
