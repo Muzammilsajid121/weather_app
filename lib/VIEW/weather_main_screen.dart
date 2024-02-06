5bd27e8acb13211de88b4e127b28f4b8e552eedc
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2024-02-06 22:19:50 +0500

    add functionalities and update UI

--- lib/VIEW/weather_main_screen.dart
+++ lib/VIEW/weather_main_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/Functions/date_time_func.dart';
 import 'package:weather_app/Services/weather_api_services.dart';
 
+
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
 
@@ -13,6 +14,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
   //Objects
   WeatherApiServices weatherApiServices = WeatherApiServices();
   final Functions function = Functions();
+  
 
   //
   @override
@@ -62,7 +64,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                 children: [
             Row(
               children: [
-                Icon(Icons.pin_drop),
+               const  Icon(Icons.pin_drop),
                  Text(
           ( weatherApiServices.location['name'].toString() + ","),
            style: Theme.of(context).textTheme.bodyLarge,
@@ -91,75 +93,109 @@ class _WeatherScreenState extends State<WeatherScreen> {
               
               ],
              ),
+      
           // Divider( height: 2, endIndent: 12, color: Colors.black,),
           //Extras
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              //WIND
-              Column(
-                children: [
-                 const  Icon(Icons.water_drop_rounded),
-            Text( weatherApiServices.current['humidity'].toString(),
-                   style: Theme.of(context).textTheme.bodyLarge,),
-             Text("Humidity", style: Theme.of(context).textTheme.bodyMedium),
-                ],
-              ),
-          
-              //Feels Like
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //WIND
                 Column(
-                children: [
-                 const  Icon(Icons.person),
-                 Text( weatherApiServices.current['feelslike_c'].toString(),
-                   style: Theme.of(context).textTheme.bodyLarge,),
-             Text("Feels Like", style: Theme.of(context).textTheme.bodyMedium),
-                ],
-              ),
-          
-               //Feels Like
-                Column(
-                children: [
-                 const  Icon(Icons.wind_power),
-                 Text( weatherApiServices.current['feelslike_c'].toString(),
-                   style: Theme.of(context).textTheme.bodyLarge,),
-             Text("Wind KPH", style: Theme.of(context).textTheme.bodyMedium),
-                ],
-              ),
-          
-              //Clouds
-                Column(
-                children: [
-                 const  Icon(Icons.cloud),
-                 Text( weatherApiServices.current['cloud'].toString(),
-                   style: Theme.of(context).textTheme.bodyLarge,),
-             Text("Cloud", style: Theme.of(context).textTheme.bodyMedium),
-                ],
-              ),
-        
-            ],
+                  children: [
+                   const  Icon(Icons.water_drop_rounded),
+              Text( weatherApiServices.current['humidity'].toString(),
+                     style: Theme.of(context).textTheme.bodyLarge,),
+               Text("Humidity", style: Theme.of(context).textTheme.bodyMedium),
+                  ],
+                ),
+               const  SizedBox(width: 20,),
+            
+                //Feels Like
+                  Column(
+                  children: [
+                   const  Icon(Icons.person),
+                   Text( weatherApiServices.current['feelslike_c'].toString(),
+                     style: Theme.of(context).textTheme.bodyLarge,),
+               Text("Feels Like", style: Theme.of(context).textTheme.bodyMedium),
+                  ],
+                ),
+         const  SizedBox(width: 20,),
+
+                 //Feels Like
+                  Column(
+                  children: [
+                   const  Icon(Icons.wind_power),
+                   Text( weatherApiServices.current['feelslike_c'].toString(),
+                     style: Theme.of(context).textTheme.bodyLarge,),
+               Text("Wind KPH", style: Theme.of(context).textTheme.bodyMedium),
+                  ],
+                ),
+                  const  SizedBox(width: 20,),
+         
+                //Clouds
+                  Column(
+                  children: [
+                   const  Icon(Icons.cloud),
+                   Text( weatherApiServices.current['cloud'].toString(),
+                     style: Theme.of(context).textTheme.bodyLarge,),
+               Text("Cloud", style: Theme.of(context).textTheme.bodyMedium),
+                  ],
+                ),
+         const  SizedBox(width: 20,),
+
+                  //SunRise
+                  Column(
+                  children: [
+                   Image.asset('assets/sunrise.png', height: 35, width: 30,),
+                   Text( 
+                   weatherApiServices.forecast['forecastday'][0]['astro']['sunrise'].toString(),
+                  style: Theme.of(context).textTheme.bodyLarge,),
+               Text("Sun Rise", style: Theme.of(context).textTheme.bodyMedium),
+                  ],
+                ),
+                const  SizedBox(width: 20,),
+           
+                  //SunSet
+                  Column(
+                  children: [
+                     Image.asset('assets/sunset.png', height: 35, width: 30,),
+                   Text(  weatherApiServices.forecast['forecastday'][0]['astro']['sunrise'].toString(),
+                     style: Theme.of(context).textTheme.bodyLarge,),
+               Text("Sun Set", style: Theme.of(context).textTheme.bodyMedium),
+                  ],
+                ),
+                // const  SizedBox(width: 12,),
+                   
+              ],
+            ),
           ),
-       
-                      ],
-                    );
-                  }
-                },
-              ),
-          
-             
-          
-          
-              SizedBox(height: height * 0.1),
-          
-              // ListViewbuilder Containers
-              SizedBox(
-                height: height * 0.20,
+ 
+const SizedBox(height: 20,),
+
+Row(mainAxisAlignment: MainAxisAlignment.center,
+
+  children: [
+   const Expanded(child:   Divider(color: Colors.black,thickness: 1, height: 1,)),
+    Text("24 Hours", style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontSize: 25)),
+   const Expanded(child:   Divider(color: Colors.black,thickness: 1, height: 1)),
+
+  ],
+),
+
+const SizedBox(height: 4,),
+
+       SizedBox(
+               
+                height: height * 0.22,
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
-                  itemCount: 8,
+                  itemCount: weatherApiServices.forecast['forecastday'][0]['hour'].length,
                   itemBuilder: (context, index) {
                     return Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 6.0),
                       child: Container(
-                        width: width * 0.20,
+                        width: width * 0.30,
                         decoration: const BoxDecoration(
                           color: Color(0xffECF3FE),
                           borderRadius: BorderRadius.all(Radius.circular(38)),
@@ -176,25 +212,45 @@ class _WeatherScreenState extends State<WeatherScreen> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            const Icon(Icons.air),
-                            SizedBox(height: height * 0.01),
-                            Text("25* C",
-                                style:
-                                    Theme.of(context).textTheme.bodyMedium),
-                            SizedBox(height: height * 0.01),
-                            Text("Monday",
-                                style:
-                                    Theme.of(context).textTheme.bodySmall),
-                            Text("8 am",
-                                style:
-                                    Theme.of(context).textTheme.bodySmall),
+
+//Hourly Image
+ Image.network(
+'https:' + weatherApiServices.forecast['forecastday'][0]['hour'][index]['condition']['icon'],
+ height: 50, width:50, ),
+
+ //Hourly Temperature          
+Text(
+  weatherApiServices.forecast['forecastday'].isNotEmpty
+      ? weatherApiServices.forecast['forecastday'][0]['hour'][index]['temp_c'].toString() +"°"
+      : 'N/A',
+ style: Theme.of(context).textTheme.bodyLarge,),
+                   
+  //Hourly Condition Text                       
+     Text(weatherApiServices.forecast['forecastday'][0]['hour'][index]['condition']['text'],
+     overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.bodySmall),
+
+   //Hourly Date with AM PM                        
+  Text(function.formatApiDateWithAmPm(
+  weatherApiServices.forecast['forecastday'][0]['hour'][index]['time'],),
+     style: Theme.of(context).textTheme.bodySmall),
                           ],
                         ),
                       ),
                     );
                   },
                 ),
+              
+            ),
+           ],
+           
+                    );
+                  }
+                 
+                },
               ),
+
+
             ],
           ),
         ),
